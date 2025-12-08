2102a28fc225d21e30fcc780743899a3888774f5
MrDanLee <daniel23lozano@gmail.com>
2025-12-08 14:10:21 +0100

    Resolución ejercicio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
-// Obtener información general
-// Obtener un pokemon 
-// Obtener info de ese pokemon
-// Iterar en una funcion
-// Insertar en DOM
+console.log('Hello!');
 
-console.log('funciona')
+/*
+nombre, imagen, tipo, altura y peso
+*/
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-.then((response) => {
-  if (!response.ok) {
-    throw new Error(response.status)
-  }
-  return response.json()
-})
-.then((data) => console.log(data))
-.catch((error) => console.log(error))
+const tittle = document.getElementById('tittle-js');
+const text = document.getElementById('text-js');
 
+const button = document.getElementById('get-pokemon');
+function renderPokemon(pokemon) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const template = `
+      <li>
+        <h3>Nombre: ${data.species.name}</h3>
+        <img src="${data.species.url}" alt="${data.species.name}">
+        <p>Tipo: ${data.types[0].type.name}</p>
+        <p>Altura: ${data.height}</p>
+        <p>Peso: ${data.weight}</p>      
+      </li>
+      `;
+      text.innerHTML = template;
+    })
+    .catch((error) => console.log(`${error}`));
+}
 
+// renderPokemon('squirtle');
+
+// select.addEventListener('click', (value) => {
+//   console.log(value.srcElement.value);
+// });
+
+button.addEventListener('click', () => {
+  const select = document.getElementById('pokemon-select');
+  console.log(select.value);
+  renderPokemon(`${select.value}`);
+});
